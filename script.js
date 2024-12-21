68344dc96bf01e88f59ab63379cbb0bddd4c6449
LivinGv1 <127312481+LivinGv1@users.noreply.github.com>
2024-12-21 09:54:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the button element by its ID
+const button = document.getElementById('alertButton');
+
+// Add an event listener to the button to show an alert when clicked
+button.addEventListener('click', function() {
+  alert('Hello! You clicked the button!');
+});
